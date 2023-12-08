ebad89d51f32b6a52cb5126bf061b25cd8e41c41
Chris Breeden <chris_breeden@discovery.com>
2023-12-08 14:09:06 -0800

    clean up test

--- backend/engine/tests/test_veracode_sbom_parser.py
+++ backend/engine/tests/test_veracode_sbom_parser.py
@@ -12,6 +12,10 @@ from engine.processor.sbom import process_sbom as flatten_sbom_and_write_to_s3
 from utils.plugin import Result
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+TEST_VERACODE_OUTPUT_PATH = os.path.join(TEST_DIR, "data", "veracode", "sample-sbom-results.json")
+EXPECTED_JSON = '[{"name": "PyLibrary", "version": "1.0.0", "licenses": [], "source": "requirements.txt", "deps": [], "type": null}, {"name": "pylibrary", "version": "1.0.0", "licenses": [{"id": "MIT", "name": "MIT license (MIT)"}], "source": "PYPI", "deps": [], "type": "pypi"}]'
+
+
 VERACODE_SBOM_RESULT = Result(
     name="Veracode SBOM",
     type="sbom",
@@ -22,20 +26,22 @@ VERACODE_SBOM_RESULT = Result(
     alerts=[],
     debug=[],
 )
-TEST_VERACODE_OUTPUT_PATH = os.path.join(TEST_DIR, "data", "veracode", "sample-sbom-results.json")
-EXPECTED_JSON = '[{"name": "PyLibrary", "version": "1.0.0", "licenses": [], "source": "requirements.txt", "deps": [], "type": null}, {"name": "pylibrary", "version": "1.0.0", "licenses": [{"id": "MIT", "name": "MIT license (MIT)"}], "source": "PYPI", "deps": [], "type": "pypi"}]'
 
 
 @patch("engine.processor.sbom.write_sbom_json", autospec=True)
 @patch("engine.processor.sbom.process_dependency")
 class TestVeracodeSbomParser(unittest.TestCase):
-    def test_raw_sbom_parse(self, _, mock_write_sbom_json):
+    def test_sbom_parse(self, _, mock_write_sbom_json):
         with open(TEST_VERACODE_OUTPUT_PATH) as f:
             output = json.load(f, parse_float=Decimal)
+
+        # process raw Veracode output in the same way SBOM plugin does
         graph = process_veracode_output(graphs=output["records"][0]["graphs"], libs=output["records"][0]["libraries"])
+
         mock_result = unittest.mock.MagicMock(side_effect=VERACODE_SBOM_RESULT)
         type(mock_result).details = PropertyMock(return_value=graph)
         mock_scan = unittest.mock.MagicMock(side_effect=Scan())
+
         flatten_sbom_and_write_to_s3(mock_result, mock_scan)
 
         # asserts that the json sent to s3 is equal to the expected json
