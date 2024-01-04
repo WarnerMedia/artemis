49a2142bb08a43e245809a3e6c721c4e20cbc333
Faizan <faizan.hussain@wbd.com>
2024-01-04 11:30:29 -0500

    Finished package-lock generator. Now generates package-lock files if package.json exists. Code still needs to be refactored/cleaned/split

--- backend/engine/plugins/trivy/main.py
+++ backend/engine/plugins/trivy/main.py
@@ -5,8 +5,9 @@ import json
 import subprocess
 from typing import NamedTuple
 import os
-
+from glob import glob
 from engine.plugins.lib import utils
+from engine.plugins.node_dependencies.write_npmrc import handle_npmrc_creation
 
 logger = utils.setup_logging("trivy")
 
@@ -14,9 +15,9 @@ DESC_REMEDIATION_SPLIT = "## Recommendation"
 NO_RESULTS_TEXT = "no supported file was detected"
 JSON_FILE = "trivy.json"
 
-def run_install(include_dev, path):
+def install_package_files(include_dev, path, root_path):
     # Create a package-lock.json file if it doesn't already exist
-    logger.info(f'Generating package-lock.json for {path} (including dev dependencies: {include_dev})')
+    logger.info(f'Generating package-lock.json for {path.replace(root_path, "")} (including dev dependencies: {include_dev})')
     cmd = [
         "npm",
         "install",
@@ -28,6 +29,43 @@ def run_install(include_dev, path):
     if not include_dev:
         cmd.append("--only=prod")
     return subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=path, check=False)
+
+def check_package_files(path: str, include_dev: bool) -> tuple:
+    """
+    Main Function
+    Find all of the package.json files in the repo and run 'npm audit' against them.
+    Parses the results and returns them with the errors.
+    """
+
+    # Find and loop through all the package.json files in the path
+    files = glob("%s/**/package.json" % path, recursive=True)
+
+    logger.info("Found %d package.json files", len(files))
+
+    # Build a set of all directories containing package files
+    paths = set()
+    for filename in files:
+        paths.add(os.path.dirname(filename))
+
+    # Write a .npmrc file based on the set of package.json files found
+    handle_npmrc_creation(paths)
+
+    for sub_path in paths:
+        results_dct = {}
+        lockfile = os.path.join(sub_path, "package-lock.json")
+        lockfile_missing = not os.path.exists(lockfile)
+        if lockfile_missing:
+            msg = (
+                f"No package-lock.json file was found in path {sub_path.replace(path, '')}. "
+                "Please consider creating a package-lock file for this project."
+            )
+            results_dct["warning"] = msg
+            r = install_package_files(include_dev, sub_path, path)
+            if r.returncode != 0:
+                logger.error(r.stderr.decode("utf-8"))
+                return {"results": results_dct, "lockfile": lockfile, "lockfile_missing": lockfile_missing}
+    # Return the results
+    return results_dct
 
 def parse_output(output: list) -> list:
     results = []
@@ -198,19 +236,8 @@ def main():
     results = []
 
     # Generate Lock files
-    results_dct = {}
-    lockfile = os.path.join(path, "package-lock.json")
-    lockfile_missing = not os.path.exists(lockfile)
-    if lockfile_missing:
-        msg = (
-            f"No package-lock.json file was found in path {path.replace(root_path, '')}. "
-            "Please consider creating a package-lock file for this project."
-        )
-        results_dct["warning"] = msg
-        r = run_install(include_dev, path, root_path)
-        if r.returncode != 0:
-            logger.error(r.stderr.decode("utf-8"))
-            return {"results": results_dct, "lockfile": lockfile, "lockfile_missing": lockfile_missing}
+    check_package_files(args.path, include_dev)
+
     # Scan local lock files
     output = execute_trivy_lock_scan(args.path, include_dev)
     logger.debug(output)
@@ -219,7 +246,7 @@ def main():
         logger.warning("Lock file output is None. Continuing.")
     else:
         result = parse_output(output["Results"])
-        logger.info("Lock file output parsed. Success: %s", not bool(result))
+        logger.info("Lock file output parsed. Success: %s", bool(result))
         results.extend(result)
 
     # Scan Images
