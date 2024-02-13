9273e44674777adc5c85aba6e7ec789699e8334b
Faizan <faizan.hussain@wbd.com>
2024-02-13 07:03:03 -0500

    black formatter

--- backend/lambdas/api/repo/tests/test_parse_event.py
+++ backend/lambdas/api/repo/tests/test_parse_event.py
@@ -230,7 +230,7 @@ EXPECTED_EVENT_RESULT_1 = {
         "-bundler_audit",
         "-php_sensio_security_checker",
         "-github_repo_health",
-        "-trivy_sbom"
+        "-trivy_sbom",
     ],
 }
 
