b3dee743a803d94f2b43b71c59b6486e5056e82e
kamsiy <kamsi.ibeziako@wbd.com>
2024-02-16 08:23:33 -0500

    fix lint issue

--- backend/engine/tests/test_owasp_dependency_check.py
+++ backend/engine/tests/test_owasp_dependency_check.py
@@ -14,7 +14,6 @@ JAVA_DIR = os.path.join(TEST_DIR, "data", "java")
 
 
 class TestBandit(unittest.TestCase):
-
     def test_parse_build_results(self):
         with open(os.path.join(DEPENDENCY_CHECK_DIR, "parsed-output.json")) as json_file:
             parsed_report = json.load(json_file)
