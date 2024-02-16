b472aff21de688ee65e36f45b68436fcf9201248
kamsiy <kamsi.ibeziako@wbd.com>
2024-02-16 08:04:51 -0500

    fix lint issue

--- backend/engine/tests/test_owasp_dependency_check.py
+++ backend/engine/tests/test_owasp_dependency_check.py
@@ -4,6 +4,7 @@ Tests Owasp Dependency Check Plugin
 
 import os
 import unittest
+import json
 
 from engine.plugins.owasp_dependency_check import main as owasp
 
@@ -11,83 +12,16 @@ TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 DEPENDENCY_CHECK_DIR = os.path.join(TEST_DIR, "data", "owasp_dependency_check/")
 JAVA_DIR = os.path.join(TEST_DIR, "data", "java")
 
-PARSED_ERROR = [
-    "org.owasp.dependencycheck.analyzer.exception.SearchException: Could not perform Node Audit analysis. Invalid payload submitted to Node Audit API.",
-    'org.owasp.dependencycheck.exception.InitializationException: Exception from bundle-audit process: java.io.IOException: Cannot run program "bundle-audit" (in directory "/tmp/dctemp2ebe5b42-3852-4a6b-8148-f0ebaa1af432"): error=2, No such file or directory. Disabling Ruby Bundle Audit Analyzer',
-]
-
-
-PARSED_VULNERABILITIES = [
-    {
-        "component": "jackson-exploit-1.0-SNAPSHOT.jar (shaded: ch.qos.logback:logback-classic:1.2.3)",
-        "source": "pom.xml",
-        "id": "CVE-2021-42550",
-        "description": "In logback version 1.2.7 and prior versions, an attacker with the required privileges to edit configurations files could craft a malicious configuration allowing to execute arbitrary code loaded from LDAP servers.",
-        "severity": "medium",
-        "remediation": "",
-        "inventory": {
-            "component": {"name": "CVE-2021-42550", "version": "", "type": "maven"},
-            "advisory_ids": ["CVE-2021-42550"],
-        },
-    },
-    {
-        "component": "jackson-exploit-1.0-SNAPSHOT.jar (shaded: ch.qos.logback:logback-classic:1.2.3)",
-        "source": "pom.xml",
-        "id": "CVE-2023-6378",
-        "description": "A serialization vulnerability in logback receiver component part of \nlogback version 1.4.11 allows an attacker to mount a Denial-Of-Service \nattack by sending poisoned data.\n\n",
-        "severity": "high",
-        "remediation": "",
-        "inventory": {
-            "component": {"name": "CVE-2023-6378", "version": "", "type": "maven"},
-            "advisory_ids": ["CVE-2023-6378"],
-        },
-    },
-    {
-        "component": "jackson-exploit-1.0-SNAPSHOT.jar (shaded: ch.qos.logback:logback-core:1.2.3)",
-        "source": "pom.xml",
-        "id": "CVE-2021-42550",
-        "description": "In logback version 1.2.7 and prior versions, an attacker with the required privileges to edit configurations files could craft a malicious configuration allowing to execute arbitrary code loaded from LDAP servers.",
-        "severity": "medium",
-        "remediation": "",
-        "inventory": {
-            "component": {"name": "CVE-2021-42550", "version": "", "type": "maven"},
-            "advisory_ids": ["CVE-2021-42550"],
-        },
-    },
-    {
-        "component": "jackson-exploit-1.0-SNAPSHOT.jar (shaded: ch.qos.logback:logback-core:1.2.3)",
-        "source": "pom.xml",
-        "id": "CVE-2023-6378",
-        "description": "A serialization vulnerability in logback receiver component part of \nlogback version 1.4.11 allows an attacker to mount a Denial-Of-Service \nattack by sending poisoned data.\n\n",
-        "severity": "high",
-        "remediation": "",
-        "inventory": {
-            "component": {"name": "CVE-2023-6378", "version": "", "type": "maven"},
-            "advisory_ids": ["CVE-2023-6378"],
-        },
-    },
-    {
-        "component": "jackson-exploit-1.0-SNAPSHOT.jar (shaded: org.eclipse.jetty:jetty-io:9.4.8.v20171121)",
-        "source": "pom.xml",
-        "id": "CVE-2021-28165",
-        "description": "In Eclipse Jetty 7.2.2 to 9.4.38, 10.0.0.alpha0 to 10.0.1, and 11.0.0.alpha0 to 11.0.1, CPU usage can reach 100% upon receiving a large invalid TLS frame.\n\nSonatype's research suggests that this CVE's details differ from those defined at NVD. See https://ossindex.sonatype.org/vulnerability/CVE-2021-28165 for details",
-        "severity": "high",
-        "remediation": "",
-        "inventory": {
-            "component": {"name": "CVE-2021-28165", "version": "", "type": "maven"},
-            "advisory_ids": ["CVE-2021-28165"],
-        },
-    },
-]
-
 
 class TestBandit(unittest.TestCase):
 
     def test_parse_build_results(self):
-        scan_report = owasp.parse_scanner_output_json(DEPENDENCY_CHECK_DIR, 0)
-
-        self.assertListEqual(scan_report["errors"].sort(), PARSED_ERROR.sort())
-        self.assertDictEqual(scan_report["output"], PARSED_VULNERABILITIES)
+        with open(os.path.join(DEPENDENCY_CHECK_DIR, "parsed-output.json")) as json_file:
+            parsed_report = json.load(json_file)
+            scan_report = owasp.parse_scanner_output_json(DEPENDENCY_CHECK_DIR, 0)
+            self.assertListEqual(scan_report["errors"], parsed_report["errors"])
+            self.assertListEqual(scan_report["output"], parsed_report["output"])
+            self.assertEqual(scan_report, parsed_report)
 
     def test_pom_exists(self):
         self.assertFalse(owasp.pom_exists(DEPENDENCY_CHECK_DIR))
