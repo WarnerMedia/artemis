4624176c21c12604209be2dde7c06d2cc8676e4a
kamsiy <kamsi.ibeziako@wbd.com>
2024-04-15 12:23:40 -0400

    Update terraform config

---
+++ backend/terraform/modules/metadata_events/variables.tf
@@ -0,0 +1,34 @@
+variable "app" {}
+
+variable "ver" {
+  description = "Application version"
+}
+
+variable "environment" {
+  description = "Environment name"
+}
+
+variable "aws_region" {
+  description = "The region in which to deploy"
+}
+
+variable "tags" {
+  type = map(any)
+}
+
+variable "lambda_architecture" {
+  description = "Architecture of lambda functions"
+}
+
+variable "lambda_runtime" {
+  description = "Runtime of the lambda functions"
+  default     = "python3.9"
+}
+
+variable "s3_analyzer_files_id" {}
+
+variable "metadata_events_queue" {}
+
+variable "metadata_events_enabled" {
+  description = "Whether to enable the Secrets Management process integration"
+}
