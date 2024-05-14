5911a6f928df931b1f0338e691c27d964495c2f3
Michael Imamura <michael.imamura@turner.com>
2024-05-14 15:28:57 -0400

    Add tests for request errors and ADO service.

--- backend/lambdas/api/system_services/tests/test_service.py
+++ backend/lambdas/api/system_services/tests/test_service.py
@@ -1,3 +1,6 @@
+import requests
+import responses
+from responses import matchers
 import unittest
 from unittest.mock import patch
 
@@ -21,5 +24,116 @@ class TestService(unittest.TestCase):
                 "auth_successful": False,
                 "auth_type": "service_account",
                 "error": "Unable to retrieve key",
+            },
+        )
+
+    @responses.activate
+    @patch("system_services.util.service.get_api_key", lambda *x, **y: "fake_key")
+    def test_timeout_error(self):
+        service_dict = {
+            "services": {"org": {"type": ServiceType.ADO, "secret_loc": "foo", "url": "http://example.com"}}
+        }
+        svc = Service("org/repo", service_dict)
+        responses.get("http://example.com/repo/_apis/projects", body=requests.Timeout())
+        actual = svc.to_dict()
+        self.assertEqual(
+            actual,
+            {
+                "service": "org/repo",
+                "service_type": ServiceType.ADO,
+                "reachable": False,
+                "auth_successful": False,
+                "auth_type": "service_account",
+                "error": "Timeout",
+            },
+        )
+
+    @responses.activate
+    @patch("system_services.util.service.get_api_key", lambda *x, **y: "fake_key")
+    def test_connect_error(self):
+        service_dict = {
+            "services": {"org": {"type": ServiceType.ADO, "secret_loc": "foo", "url": "http://example.com"}}
+        }
+        svc = Service("org/repo", service_dict)
+        # responses will throw ConnectionError on any unregistered URL.
+        actual = svc.to_dict()
+        self.assertEqual(
+            actual,
+            {
+                "service": "org/repo",
+                "service_type": ServiceType.ADO,
+                "reachable": False,
+                "auth_successful": False,
+                "auth_type": "service_account",
+                "error": "Connection error",
+            },
+        )
+
+    @responses.activate
+    @patch("system_services.util.service.get_api_key", lambda *x, **y: "fake_key")
+    def test_generic_request_error(self):
+        service_dict = {
+            "services": {"org": {"type": ServiceType.ADO, "secret_loc": "foo", "url": "http://example.com"}}
+        }
+        svc = Service("org/repo", service_dict)
+        # All request errors other than Timeout or ConnectionError are treated
+        # as generic "Request Failed" errors.
+        responses.get("http://example.com/repo/_apis/projects", body=requests.TooManyRedirects())
+        actual = svc.to_dict()
+        self.assertEqual(
+            actual,
+            {
+                "service": "org/repo",
+                "service_type": ServiceType.ADO,
+                "reachable": False,
+                "auth_successful": False,
+                "auth_type": "service_account",
+                "error": "Request failed",
+            },
+        )
+
+    @responses.activate
+    @patch("system_services.util.service.get_api_key", lambda *x, **y: "fake_key")
+    def test_ado_auth_failed(self):
+        service_dict = {
+            "services": {"org": {"type": ServiceType.ADO, "secret_loc": "foo", "url": "http://example.com"}}
+        }
+        svc = Service("org/repo", service_dict)
+        responses.get("http://example.com/repo/_apis/projects", status=403)
+        actual = svc.to_dict()
+        self.assertEqual(
+            actual,
+            {
+                "service": "org/repo",
+                "service_type": ServiceType.ADO,
+                "reachable": True,
+                "auth_successful": False,
+                "auth_type": "service_account",
+                "error": None,
+            },
+        )
+
+    @responses.activate
+    @patch("system_services.util.service.get_api_key", lambda *x, **y: "fake_key")
+    def test_ado_success(self):
+        service_dict = {
+            "services": {"org": {"type": ServiceType.ADO, "secret_loc": "foo", "url": "http://example.com"}}
+        }
+        svc = Service("org/repo", service_dict)
+        responses.get(
+            "http://example.com/repo/_apis/projects",
+            status=200,
+            match=[matchers.header_matcher({"Authorization": "Basic fake_key"})],
+        )
+        actual = svc.to_dict()
+        self.assertEqual(
+            actual,
+            {
+                "service": "org/repo",
+                "service_type": ServiceType.ADO,
+                "reachable": True,
+                "auth_successful": True,
+                "auth_type": "service_account",
+                "error": None,
             },
         )
