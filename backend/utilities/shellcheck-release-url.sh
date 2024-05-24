88051958a3d56f37fbccbe0cf5da8668d245aab9
Michael Imamura <michael.imamura@turner.com>
2024-05-24 13:30:39 -0400

    Avoid re-downloading external tools used in backend unit tests (#151)
    * Don't re-download Trivy.
    
    * Reinstall mock swiftlint if source changes.
    
    * Don't re-download shellcheck.
    
    * Download correct ShellCheck for OS and platform.
    
    This fixes the x86_64 version being downloaded on Apple Silicon Macs.
    
    * Reinstall Detekt wrapper if it changes.
    
    * Include version in unit test tool dirs.
    
    This triggers the tool to be downloaded if the version changes.
    
    * Cache the downloaded tools used in unit tests.
    
    * Include runner architecture in cache key.

---
+++ backend/utilities/shellcheck-release-url.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Generate the download URL for a ShellCheck release tarball for the specified version.
+# This takes into account the current OS and architecture.
+
+set -o pipefail
+
+readonly version="$1"; shift
+if [[ $version == '' ]]; then
+  echo "Usage: $0 version" >&2
+  exit 1
+fi
+
+platform="$(uname | tr '[:upper:]' '[:lower:]')" || exit 1
+if [[ $platform != 'darwin' && $platform != 'linux' ]]; then
+  echo "Unsupported ShellCheck release platform: $platform" >&2
+  exit 1
+fi
+readonly platform
+
+arch="$(uname -m)" || exit 1
+case "$arch" in
+  aarch64|x86_64) ;;
+  arm64) arch=aarch64 ;;
+  *)
+    echo "Unsupported ShellCheck release architecture: $arch" >&2
+    exit 1
+esac
+readonly arch
+
+echo "https://github.com/koalaman/shellcheck/releases/download/v${version}/shellcheck-v${version}.${platform}.${arch}.tar.xz"
