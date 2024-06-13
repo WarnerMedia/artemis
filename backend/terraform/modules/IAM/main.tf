cb60b11338cecc0bc2438761c61e22aaad3a83f7
Michael Imamura <michael.imamura@turner.com>
2024-06-13 15:45:41 -0400

    Fix Terraform formatting.

--- backend/terraform/modules/IAM/main.tf
+++ backend/terraform/modules/IAM/main.tf
@@ -1,5 +1,5 @@
 
 resource "aws_accessanalyzer_analyzer" "access_analyzer" {
   analyzer_name = "${var.app}-access-analyzer"
-  tags = var.tags
+  tags          = var.tags
 }
--- backend/terraform/modules/vpc/variables.tf
+++ backend/terraform/modules/vpc/variables.tf
@@ -1,5 +1,5 @@
 variable "tags" {
-  type = map
+  type = map(any)
 }
 
 variable "vpc_cidr" {
