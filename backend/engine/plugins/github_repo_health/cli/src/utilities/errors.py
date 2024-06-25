f398f983af3cd387195e920189d17d2181493a8d
Garrett Marconet <garrett.marconet@warnermedia.com>
2024-06-25 10:42:39 -0700

    remove unused exception classes

--- backend/engine/plugins/github_repo_health/cli/src/utilities/errors.py
+++ backend/engine/plugins/github_repo_health/cli/src/utilities/errors.py
@@ -4,19 +4,3 @@ from enum import Enum
 class ErrorCode(int, Enum):
     UNEXPECTED = 1
     NOT_FOUND = 2
-
-
-class CodedException(Exception):
-    def __init__(self, message, code):
-        super().__init__(message)
-        self.code = code
-
-
-class GetRepositoryException(CodedException):
-    def __init__(self, message):
-        super().__init__(message, ErrorCode.NOT_FOUND)
-
-
-class GetBranchException(CodedException):
-    def __init__(self, message):
-        super().__init__(message, ErrorCode.NOT_FOUND)
