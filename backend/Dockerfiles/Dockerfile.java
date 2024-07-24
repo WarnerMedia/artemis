06894253b16980631c95836d54d6c285a9c88093
Michael Imamura <michael.imamura@turner.com>
2024-07-24 15:49:44 -0400

    Download tools in a common stage.
    OWASP FindSecBugs and Detekt are included in all Java images, so we now
    download them into a common build stage which can be cached and shared
    between dist targets.
    
    Dependency Check is not included since it is currently only enabled for
    the Java 8 image, and we don't know if the "updateonly" step is portable
    between Java versions.

--- backend/Dockerfiles/Dockerfile.java
+++ backend/Dockerfiles/Dockerfile.java
@@ -1,21 +1,45 @@
+# syntax=docker/dockerfile:1
+
 ARG OPENJDK_VER=7u201-jdk-alpine3.9
-FROM openjdk:$OPENJDK_VER
+
+# Common tools included in all Java targets.
+FROM alpine:3.20 AS common
 
 # We're using Alpine's /bin/sh, so disable pipefail suggestion.
 # hadolint global ignore=DL4006
 
-ARG MAINTAINER
-LABEL maintainer=$MAINTAINER
-
 ARG DETEKT_VER
 ARG DETEKT_SHA
-ARG FSB_PATCH=""
-ARG FSB_VER=1.9.0
+ARG FSB_VER
+ARG FSB_PATCH
+
+WORKDIR /app
+
+# Install OWASP Find Security Bugs and fix up launcher script.
+RUN wget -q -O /tmp/findsecbugs.zip "https://github.com/find-sec-bugs/find-sec-bugs/releases/download/version-$FSB_VER/findsecbugs-cli-$FSB_VER$FSB_PATCH.zip" && \
+    unzip /tmp/findsecbugs.zip -d /app/findsecbugs && \
+    sed -i -e 's/\r$//' /app/findsecbugs/findsecbugs.sh && \
+    chmod a+x /app/findsecbugs/findsecbugs.sh && \
+    rm /tmp/findsecbugs.zip
+
+# Install detekt.
+RUN wget -q -O /usr/local/bin/detekt.jar \
+        "https://github.com/detekt/detekt/releases/download/v${DETEKT_VER}/detekt-cli-${DETEKT_VER}-all.jar" && \
+    echo "$DETEKT_SHA  /usr/local/bin/detekt.jar" | sha256sum -c -
+## The detekt wrapper script is renamed from detekt.sh to "detekt"
+## This is for compatability with installations done via a package manager
+COPY ./engine/plugins/detekt/detekt.sh /usr/local/bin/detekt
+RUN chmod a+x /usr/local/bin/detekt
+
+
+# Java-version-specific images.
+FROM openjdk:$OPENJDK_VER AS dist
+
 ARG OWASP_DC=""
-ARG OWASP_DC_SHA
-ENV FSB_VER=$FSB_VER
-ENV FSB_PATCH=$FSB_PATCH
-ENV OWASP_DC=$OWASP_DC
+ARG OWASP_DC_SHA=""
+
+ARG MAINTAINER
+LABEL maintainer=$MAINTAINER
 
 # Base apk requirements to execute script
 # hadolint ignore=DL3018
@@ -42,25 +66,12 @@ WORKDIR /app
 # downloading, update dependency check data so it doesn't have to all be
 # downloaded each scan
 RUN if [ "$OWASP_DC" != "" ] ; then \
-    wget -q -O /tmp/dependency-check.zip https://github.com/jeremylong/DependencyCheck/releases/download/v$OWASP_DC/dependency-check-$OWASP_DC-release.zip && \
+    wget -q -O /tmp/dependency-check.zip "https://github.com/jeremylong/DependencyCheck/releases/download/v$OWASP_DC/dependency-check-$OWASP_DC-release.zip" && \
     echo "$OWASP_DC_SHA  /tmp/dependency-check.zip" | sha256sum -c - && \
     unzip /tmp/dependency-check.zip -d /app/owasp_dependency-check && \
     rm /tmp/dependency-check.zip && \
     /app/owasp_dependency-check/dependency-check/bin/dependency-check.sh --connectiontimeout 120000 --updateonly ; \
     fi
 
-# Install OWASP Find Security Bugs and fix up launcher script.
-RUN wget -q -O /tmp/findsecbugs.zip https://github.com/find-sec-bugs/find-sec-bugs/releases/download/version-$FSB_VER/findsecbugs-cli-$FSB_VER$FSB_PATCH.zip && \
-    unzip /tmp/findsecbugs.zip -d /app/findsecbugs && \
-    sed -i -e 's/\r$//' /app/findsecbugs/findsecbugs.sh && \
-    chmod a+x /app/findsecbugs/findsecbugs.sh && \
-    rm /tmp/findsecbugs.zip
-
-# Install detekt
-RUN wget -q -O /usr/local/bin/detekt.jar \
-        "https://github.com/detekt/detekt/releases/download/v${DETEKT_VER}/detekt-cli-${DETEKT_VER}-all.jar" && \
-    echo "$DETEKT_SHA  /usr/local/bin/detekt.jar" | sha256sum -c -
-## The detekt wrapper script is renamed from detekt.sh to "detekt"
-## This is for compatability with installations done via a package manager
-COPY ./engine/plugins/detekt/detekt.sh /usr/local/bin/detekt
-RUN chmod a+x /usr/local/bin/detekt
+COPY --from=common /app/findsecbugs/ /app/findsecbugs/
+COPY --from=common /usr/local/bin/detekt* /usr/local/bin
