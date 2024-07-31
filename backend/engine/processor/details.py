5299cc9d95ec7df825be6f8636b5107f80168cbb
Matt Fleury <matt.fleury@wbd.com>
2024-07-31 11:31:56 -0400

    adding archive flag to payload

--- backend/engine/processor/details.py
+++ backend/engine/processor/details.py
@@ -3,6 +3,8 @@ class Details:
         self.repo = details["repo"]
         self.org = self.repo.split("/")[0]
         self.service = details.get("service", "github")
+        self.isPublic = details.get("public", False)
+        self.isArchived = details.get("archived", False)
         self.callback_url = details.get("callback", {}).get("url")
         self.client_id = details.get("callback", {}).get("client_id")
         # Feature flags of the User/API key
