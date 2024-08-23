1c1bf8e7cd71cfd8c666a51922cb76257186e76a
kamsiy <kamsi.ibeziako@wbd.com>
2024-08-23 03:04:29 -0400

    update terraform

--- backend/terraform/modules/secrets_management/main.tf
+++ backend/terraform/modules/secrets_management/main.tf
@@ -15,6 +15,13 @@ resource "aws_lambda_function" "secrets-handler" {
   architectures = [var.lambda_architecture]
   timeout       = 30
 
+  layers = [aws_lambda_layer_version.backend_core.arn]
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to the layers as the CI pipline will deploy newer versions
+      layers
+    ]
+  }
   role = aws_iam_role.secrets-role.arn
 
   environment {
