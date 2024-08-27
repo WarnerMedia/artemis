ad49693c5a3f41162213ce5a0d6f121cf84826d7
Kamsi Ibeziako <kamsi.ibeziako@wbd.com>
2024-08-27 10:05:14 -0400

    Revert updates to setup.py (#249)
    * Revert updates to setup.py
    
    * lint
    
    * Update pipenv version and setuptools

--- backend/libs/artemislib/setup.py
+++ backend/libs/artemislib/setup.py
@@ -21,7 +21,14 @@ setup(
     url=("https://github.com/warnermedia/artemis/backend/libs/artemislib"),
     packages=find_packages(),
     setup_requires=["pytest-runner"],
-    install_requires=[],
+    install_requires=[
+        "boto3~=1.34",
+        "boto3-stubs[ec2,lambda,s3,secretsmanager,sqs]~=1.34",
+        # pyjwt requires the cryptography library but it needs to be installed
+        # separately because it contains platform-dependent pre-compiled code
+        "pyjwt[crypto]~=2.8",
+        "requests~=2.31",
+    ],
     tests_require=["pytest~=8.2"],
     classifiers=[
         "Programming Language :: Python :: 3.9",
