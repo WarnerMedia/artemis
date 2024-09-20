ccf0467c0afed5589632d5be3e6712dd9fdd0fc8
kamsiy <kamsi.ibeziako@wbd.com>
2024-09-19 21:15:37 -0400

    revert the revert

--- backend/engine/plugins/lib/utils.py
+++ backend/engine/plugins/lib/utils.py
@@ -3,7 +3,6 @@ import json
 import logging
 import os
 import subprocess
-import sys
 
 import boto3
 from botocore.exceptions import ClientError, NoCredentialsError
@@ -15,13 +14,53 @@ APPLICATION = os.environ.get("APPLICATION", "artemis")
 REGION = os.environ.get("REGION", "us-east-2")
 
 
+class JSONFormatter(logging.Formatter):
+    """
+    Formats a LogRecord to a JSON encoded string.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.reserved_attrs = set(logging.LogRecord("", 0, "", 0, "", (), None).__dict__.keys())
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = record.__dict__.copy()
+        log_record = {
+            "timestamp": self.formatTime(record, self.datefmt),
+            "level": record.levelname,
+            "location": f"{record.funcName}:{record.lineno}",
+            "message": record.getMessage(),
+        }
+        # Exclude reserved attributes and Add additional_fields
+        for key, value in message.items():
+            if key not in self.reserved_attrs and value:
+                log_record[key] = value
+
+        # Format Exceptions
+        if record.exc_info and record.exc_text is None:
+            record.exc_text = self.formatException(record.exc_info)
+
+        if record.exc_text:
+            log_record["exc_info"] = record.exc_text
+
+        if record.stack_info:
+            log_record["stack_info"] = self.formatStack(record.stack_info)
+
+        log_record = self._prune_null_fields(log_record)
+        return json.dumps(log_record)
+
+    def _prune_null_fields(self, records: dict) -> dict:
+        """
+        Remove any key with Null values
+        """
+        return {k: v for k, v in records.items() if v is not None}
+
+
 def setup_logging(name):
-    log = logging.getLogger(__name__)
+    log = logging.getLogger(name)
     if not log.handlers:
+        formatter = JSONFormatter()
         console = logging.StreamHandler()
-        formatter = logging.Formatter(
-            fmt=f"%(asctime)s %(levelname)-8s [{name}] %(message)s", datefmt="[%Y-%m-%dT%H:%M:%S%z]"
-        )
         console.setFormatter(formatter)
         log.addHandler(console)
         log.setLevel(logging.INFO)
