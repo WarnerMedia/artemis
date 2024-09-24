86e118dbd0eac02ea345d55050ac349f4218d0e1
Faizan <faizan.hussain@wbd.com>
2024-09-24 19:06:05 -0400

    change log type

--- backend/engine/utils/engine.py
+++ backend/engine/utils/engine.py
@@ -23,7 +23,7 @@ def check_disk_space(repo_size: int, available_space=None) -> bool:
     if available_space is None:
         s = os.statvfs(".")
         available_space = (s.f_frsize * s.f_bavail) / 1024
-    log.debug(f"available space ********: {available_space}")
+    log.error(f"available space ********: {available_space}")
 
     # The worst repo encountered (so far) is twice the size on disk
     if (repo_size * 2) >= available_space:
