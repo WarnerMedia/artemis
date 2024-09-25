a430bc8b5f0b4d8f2b32bbf2c27f0f701d9902e0
Faizan <faizan.hussain@wbd.com>
2024-09-24 20:17:29 -0400

    adding more logging

--- backend/engine/utils/engine.py
+++ backend/engine/utils/engine.py
@@ -20,6 +20,7 @@ def check_disk_space(repo_size: int, available_space=None) -> bool:
     :param repo_size: size of the repository in KB
     :return: True if the available space is greater than the repo size * 2, otherwise False
     """
+    log.error(f"CURRENT WORKING DIR: {os.getcwd()}")
     if available_space is None:
         s = os.statvfs("/cloned_repos")
         available_space = (s.f_frsize * s.f_bavail) / 1024
