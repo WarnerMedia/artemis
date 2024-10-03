7dbade1c329e87fea2d1139ebf233ac7df6b5eab
Michael Imamura <michael.imamura@turner.com>
2024-10-03 17:05:08 -0400

    Build a self-contained core plugin bundle.
    This is an alternative method of running plugins within a container.
    
    Instead of relying on the system Python and dependencies in the target
    container, this bundles the plugins, dependencies, and a "portable"
    Python distribution into a single self-contained executable.
    
    This means that the target container no longer needs a system Python
    installed and we don't need to make sure the dependencies are installed
    in the container (which can get out of sync with the engine
    dependencies).
    
    The only new requirement of the target container is that it must be a
    glibc-based distribution since the bundle contains a glibc-based Python
    and shared libraries.

---
+++ backend/engine/plugins/__main__.py
@@ -0,0 +1,29 @@
+import os
+import re
+import runpy
+import sys
+
+# Note: All messages must be set to stderr;
+#       only plugin results may be sent to stdout.
+
+if os.getenv("ARTEMIS_PLUGIN_DEBUG", False):
+    pyver = f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+    print(f"Using Python {pyver}: {sys.executable}", file=sys.stderr)
+
+name_rx = re.compile("^[a-z0-9][_a-z0-9]*$", re.IGNORECASE)
+
+if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} (plugin) [args...]", file=sys.stderr)
+    sys.exit(1)
+
+name = sys.argv[1]
+del sys.argv[1]
+
+if name != "lib" and name_rx.match(name):
+    # Run the requested plugin.
+    # This is kept compatible with the "traditional" way the plugins were
+    # run (via "python path/to/plugin/main.py").
+    runpy.run_module(f"engine.plugins.{name}.main", {}, "__main__")
+else:
+    print(f"{sys.argv[0]}: Invalid plugin: {name}", file=sys.stderr)
+    sys.exit(1)
