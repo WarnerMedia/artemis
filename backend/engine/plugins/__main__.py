6ce6f1ece935c869a1de33841db0150b08f0214b
Michael Imamura <michael.imamura@turner.com>
2024-10-08 13:48:22 -0400

    Refactor plugin name validation.
    Moved the plugin name validation out from the runner into the a util
    function so we can better document and unit test it (and re-use it in
    some other places).

--- backend/engine/plugins/__main__.py
+++ backend/engine/plugins/__main__.py
@@ -6,9 +6,10 @@ passed to the plugin.
 """
 
 import os
-import re
 import runpy
 import sys
+
+from .lib.utils import validate_plugin_name
 
 # Note: All messages must be set to stderr;
 #       only plugin results may be sent to stdout.
@@ -17,8 +18,6 @@ if os.getenv("ARTEMIS_PLUGIN_DEBUG", False):
     pyver = f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
     print(f"Using Python {pyver}: {sys.executable}", file=sys.stderr)
 
-name_rx = re.compile("^[a-z0-9][_a-z0-9]*$", re.IGNORECASE)
-
 if len(sys.argv) < 2:
     print(f"Usage: {sys.argv[0]} (plugin) [args...]", file=sys.stderr)
     sys.exit(1)
@@ -26,7 +25,7 @@ if len(sys.argv) < 2:
 name = sys.argv[1]
 del sys.argv[1]
 
-if name != "lib" and name_rx.match(name):
+if validate_plugin_name(name):
     # Run the requested plugin.
     # This is kept compatible with the "traditional" way the plugins were
     # run (via "python path/to/plugin/main.py").
