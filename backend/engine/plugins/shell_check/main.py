70e67a8b273d1c73f86200a722421c539e21c8f7
Michael Imamura <michael.imamura@turner.com>
2024-10-16 14:38:37 -0400

    ShellCheck plugin simplification and cleanups (#310)
    * Pass -S option to shellcheck.
    
    ShellCheck now supports the -S option to set the minimum severity level.
    
    * Add test case for get_files().
    
    * Search for files with native Python functions.
    
    This fixes the brittle use of calling "find" in a subprocess to get the
    list of files to scan.
    
    * Add typehints and handle error case.
    
    * Fix capitalization in docstring.
    
    * Remove duplicate extension.

--- backend/engine/plugins/shell_check/main.py
+++ backend/engine/plugins/shell_check/main.py
@@ -3,45 +3,38 @@ shell_check plugin
 """
 
 import json
+import pathlib
 import subprocess
+from typing import Any, Optional
 
 from engine.plugins.lib import utils
 
 logger = utils.setup_logging("shell_check")
 
+# Filename extensions to include in scanning.
+INCLUDE_EXTS = {".sh", ".bash", ".ksh", ".bashrc", ".bash_login", ".bash_logout", ".bash_profile"}
 
-def get_files(path):
-    """Gets all files matching what Shell Check can scan.
-    NOTE: This subprocess uses shell. DO NOT add arguments to this subprocess to avoid shell injection vulnerabilities.
-    Reason: the find command is incredibly fast but I was not able to perform globbing outside of a shell.
-    Please periodically check the wiki for any recursion feature improvements in shellcheck:
-    https://github.com/koalaman/shellcheck/wiki/Recursiveness
+
+def get_files(path: str) -> list[str]:
     """
-    proc = subprocess.run(
-        [
-            "find . -type f \\( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' -o -name '*.bashrc' -o -name "
-            "'*.bash_profile' -o -name '*.bash_login' -o -name '*.bash_logout' \\)"
-        ],
-        cwd=path,
-        capture_output=True,
-        check=False,
-        shell=True,
-    )
-    if proc.stdout:
-        files = proc.stdout.decode("utf-8").split("\n")
-        if "" in files:
-            files.remove("")
-        return files
-    return []
+    Recursively finds all files matching what can be scanned, starting from the
+    base path.
 
-
-def run_shellcheck(files, path):
-    """Runs Shellcheck across the whole project space.
-    NOTE: During Testing, the Linux shellcheck 0.71 binary did not have the severity argument (-S)
-    If upgrading remedies this, add ["-S", "error"] to the args and remove the level check in parse_output()
+    The returned filenames are relative to the base path, with "./" prepended
+    (to match prior versions).
     """
+    base = pathlib.Path(path)
+    return [
+        f"./{f.relative_to(base)}"
+        for f in base.glob("**/*")
+        if f.is_file() and (f.name in INCLUDE_EXTS or f.suffix in INCLUDE_EXTS)
+    ]
 
-    args = ["shellcheck", "-f", "json"] + files
+
+def run_shellcheck(files: list[str], path: str) -> Optional[list[Any]]:
+    """Runs ShellCheck across the whole project space."""
+
+    args = ["shellcheck", "-f", "json", "-S", "error"] + files
     proc = subprocess.run(args, cwd=path, capture_output=True, check=False)
 
     if proc.stdout:
@@ -50,11 +43,9 @@ def run_shellcheck(files, path):
         logger.error(proc.stderr.decode("utf-8"))
 
 
-def parse_output(output):
+def parse_output(output: list[Any]) -> list[dict[str, str]]:
     result = []
     for item in output:
-        if item["level"] != "error":
-            continue
         result.append(
             {
                 "filename": item["file"],
@@ -67,17 +58,17 @@ def parse_output(output):
     return result
 
 
-def main(path=None):
+def main(path: Optional[str] = None):
     logger.info("Executing Shell Check")
     if not path:
         args = utils.parse_args()
-        path = args.path
+        path = str(args.path)
     files = get_files(path)
+    result = []
     if files:
         response = run_shellcheck(files, path)
-        result = parse_output(response)
-    else:
-        result = []
+        if response:
+            result = parse_output(response)
 
     output = {"success": not bool(result), "details": result}
 
