d25e38358b2224b5087c177a1da417cede1683cc
Garrett Marconet <garrett.marconet@warnermedia.com>
2024-11-20 13:20:35 -0800

    factor out write

--- backend/engine/plugins/lib/write_npmrc.py
+++ backend/engine/plugins/lib/write_npmrc.py
@@ -98,7 +98,11 @@ def write_npmrc(logger, npmrc, scope_list: list) -> None:
     with open(npmrc, "a") as f:
         for scope in scope_list:
             logger.info(f"Writing {scope['scope']} config to %s", npmrc)
+            
             if "authToken" in scope:
-                f.write(build_npm_auth_token_config(**scope))
+                npm_config = build_npm_auth_token_config(**scope)
             else:
-                f.write(build_npm_config(**scope))
+                npm_config = build_npm_config(**scope)
+            
+            f.write(npm_config)
+            
