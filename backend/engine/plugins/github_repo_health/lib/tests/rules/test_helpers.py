bba5fd10a75dd94f6b7cab2b84c7a693e8cc49f9
Garrett Marconet <garrett.marconet@warnermedia.com>
2024-11-21 13:05:51 -0800

    add test for docs url

---
+++ backend/engine/plugins/github_repo_health/lib/tests/rules/test_helpers.py
@@ -0,0 +1,36 @@
+import unittest
+
+from github_repo_health.rules import add_metadata, BranchProtectionCommitSigning
+
+OWNER = "owner"
+REPO = "repo"
+BRANCH = "branch"
+
+DOCS_URL = "http://example.com"
+
+
+class TestHelpers(unittest.TestCase):
+    def test_add_metadata_should_not_have_docs_url_if_not_provided(self):
+        config = {}
+        result = add_metadata(True, BranchProtectionCommitSigning, config)
+
+        expected = {
+            "id": BranchProtectionCommitSigning.identifier,
+            "name": BranchProtectionCommitSigning.name,
+            "description": BranchProtectionCommitSigning.description,
+        }
+
+        self.assertEqual(expected, result)
+
+    def test_add_metadata_should_have_docs_url_if_provided(self):
+        config = { "docs_url": DOCS_URL }
+        result = add_metadata(True, BranchProtectionCommitSigning, config)
+
+        expected = {
+            "id": BranchProtectionCommitSigning.identifier,
+            "name": BranchProtectionCommitSigning.name,
+            "description": BranchProtectionCommitSigning.description,
+            "docs_url": DOCS_URL,
+        }
+
+        self.assertEqual(expected, result)
