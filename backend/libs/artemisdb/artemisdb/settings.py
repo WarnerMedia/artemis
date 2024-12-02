6c2dc0edfd364fd6acfd2ac888be048e3758ad64
Garrett Marconet <garrett.marconet@warnermedia.com>
2024-12-02 15:57:49 -0800

    upgrade to django 4.2

--- backend/libs/artemisdb/artemisdb/settings.py
+++ backend/libs/artemisdb/artemisdb/settings.py
@@ -25,7 +25,6 @@ MIGRATION_MODULES = {"artemisdb": "artemisdb.artemisdb.migrations"}
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
-USE_L10N = True
 USE_TZ = True
 
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.PBKDF2PasswordHasher"]
