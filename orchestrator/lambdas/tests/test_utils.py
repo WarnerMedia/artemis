500887ad87cb7463fb8264238b21c02ef2bc9d68
Garrett Marconet <garrett.marconet@warnermedia.com>
2025-02-05 14:15:55 -0800

    ruff format

--- orchestrator/lambdas/tests/test_utils.py
+++ orchestrator/lambdas/tests/test_utils.py
@@ -30,15 +30,11 @@ class TestUtils(unittest.TestCase):
     def test_is_valid_timestamp_utc(self):
         # Checks relative to current time plus a max scan age.
         # Make sure to change this before 9998
-        time = "9999-01-01T00:00:00Z";
-
-        self.assertTrue(utils.is_valid_timestamp(time));
+        time = "9999-01-01T00:00:00Z"
+        self.assertTrue(utils.is_valid_timestamp(time))
 
     def test_is_valid_timestamp_localized(self):
         # Checks relative to current time minus a max scan age.
         # Make sure to change this before 9998
-        time = "9999-01-01T00:00:00-08:00";
-
-        self.assertTrue(utils.is_valid_timestamp(time));
-
-        
+        time = "9999-01-01T00:00:00-08:00"
+        self.assertTrue(utils.is_valid_timestamp(time))
