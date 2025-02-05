a2db59b404530450146b332ee92949b715392676
kamsiy <kamsi.ibeziako@wbd.com>
2025-02-05 11:44:09 -0500

    Add security headers to api responses

--- backend/libs/artemisapi/artemisapi/response.py
+++ backend/libs/artemisapi/artemisapi/response.py
@@ -3,6 +3,14 @@ from http import HTTPStatus
 
 from artemisapi.const import DEFAULT_RESPONSE_MESSAGE_OVERRIDES
 
+SECURITY_HEADERS = {
+    "Strict-Transport-Security": "max-age=31536000; includeSubDomains; preload",
+    "X-Content-Type-Options": "nosniff",
+    "X-Frame-Option": "SAMEORIGIN",
+    "X-XSS-Protection": "1; mode=block",
+    "Referrer-Policy": "strict-origin-when-cross-origin",
+}
+
 
 def response(msg=None, code: HTTPStatus = HTTPStatus.OK, json_body=True, headers=None, content_type=None):
     if headers is None:
@@ -11,6 +19,8 @@ def response(msg=None, code: HTTPStatus = HTTPStatus.OK, json_body=True, headers
         headers.update({"Content-Type": "application/json" if json_body else "text/html"})
     else:
         headers.update({"Content-Type": content_type})
+
+    headers.update(SECURITY_HEADERS)
 
     if msg is None and code != HTTPStatus.NO_CONTENT:
         if code in DEFAULT_RESPONSE_MESSAGE_OVERRIDES:
