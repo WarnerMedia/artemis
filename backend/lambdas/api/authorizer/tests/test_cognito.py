03adc8d9be266a913c626cfcf8459008a116546d
faizanH <faizan.hussain@wbd.com>
2025-06-11 16:55:55 -0400

    fixing bug in tests

--- backend/lambdas/api/authorizer/tests/test_cognito.py
+++ backend/lambdas/api/authorizer/tests/test_cognito.py
@@ -1,9 +1,8 @@
 import unittest
 
 from joserfc import jwk
-import responses
 from joserfc.errors import MissingKeyError
-
+import responses
 from authorizer.cognito import load_cognito_public_keys
 
 EXAMPLE_REGION = "us-east-1"
@@ -50,5 +49,5 @@ class TestCognito(unittest.TestCase):
             EXPECTED_COGNITO_PUBLIC_KEY_URL,
             body=b'{"keys":[]}',
         )
-        with pytest.raises(MissingKeyError):
+        with self.assertRaises(MissingKeyError):
             load_cognito_public_keys(EXAMPLE_REGION, EXAMPLE_USERPOOL_ID)
