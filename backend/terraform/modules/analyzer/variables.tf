869927cfff4471f9bf5a56ac8f35ae0d08dc3b02
Kamsi <kamsi.ibeziako@wbd.com>
2025-07-14 09:22:51 -0400

    Update Docker Images & Lambda Layers to Python 3.12 (#494)
    * Update the docker containers to 3.12
    
    * Update manifest files
    
    * Migrate from packaged to uv to build boxed plugins.
    
    Significantly improves the speed and debuggability when building the
    boxed plugins:
    
    - Uses uv to download and install the standalone Python distribution.
    - Uses "uv pip" to install the dependencies. This is generally faster
      than using pip directly, and produces easier-to-debug error messages.
    - Skips the pack/unpack steps.
    
    * Rever urllib3 update
    
    * Lock dependencies
    
    * Update lambda runtimes
    
    * Update manifest files for github_repo_health
    
    * Update CI
    
    * Update README
    
    ---------
    
    Co-authored-by: Michael Imamura <michael.imamura@turner.com>

--- backend/terraform/modules/analyzer/variables.tf
+++ backend/terraform/modules/analyzer/variables.tf
@@ -204,7 +204,7 @@ variable "lambda_architecture" {
 
 variable "lambda_runtime" {
   description = "Runtime of the lambda functions"
-  default     = "python3.9"
+  default     = "python3.12"
 }
 
 variable "vpc_id" {}
--- backend/terraform/modules/secrets_management/variables.tf
+++ backend/terraform/modules/secrets_management/variables.tf
@@ -27,7 +27,7 @@ variable "lambda_architecture" {
 
 variable "lambda_runtime" {
   description = "Runtime of the lambda functions"
-  default     = "python3.9"
+  default     = "python3.12"
 }
 
 variable "s3_analyzer_files_id" {}
