429fdc15087af9f772be11428b21913bfbf1e267
kamsiy <kamsi.ibeziako@wbd.com>
2025-09-15 12:38:38 -0400

    Remove connection tests from services lambda

--- backend/lambdas/api/system_services/system_services/util/const.py
+++ backend/lambdas/api/system_services/system_services/util/const.py
@@ -1,18 +1,5 @@
 from enum import Enum
 
 
-class AuthType(Enum):
-    APP = "app"
-    SVC = "service_account"
-
-
 class Resources(str, Enum):
     STATS = "stats"
-
-
-class ServiceType(str, Enum):
-    ADO = "ado"
-    BITBUCKET_V1 = "bitbucket_v1"
-    BITBUCKET_V2 = "bitbucket_v2"
-    GITHUB = "github"
-    GITLAB = "gitlab"
--- backend/lambdas/api/system_services/system_services/util/service.py
+++ backend/lambdas/api/system_services/system_services/util/service.py
@@ -1,22 +1,10 @@
-import base64
-import functools
-import requests
 import json
 from typing import TypedDict, Literal, Optional
 from artemisdb.artemisdb.models import Repo, Scan, User
 from artemislib.datetime import format_timestamp
-from artemislib.github.app import GithubApp
 from artemislib.logging import Logger
 from artemislib.memcached import get_memcache_client
-from system_services.util.const import AuthType, ServiceType
-from system_services.util.env import (
-    APPLICATION,
-    SERVICE_AUTH_CHECK_TIMEOUT,
-    REV_PROXY_DOMAIN_SUBSTRING,
-    REV_PROXY_SECRET,
-    REV_PROXY_SECRET_HEADER,
-    REV_PROXY_SECRET_REGION,
-)
+from artemislib.services import ServiceType, AuthType
 
 log = Logger(__name__)
 
@@ -43,22 +31,13 @@ class Service:
         else:
             self._service_name = self.name
         self._service = services_dict["services"][self._service_name]
+        self._auth_type = AuthType.SVC
 
         # Update Bitbucket Service Type
         if self._service["type"] in ServiceType.BITBUCKET_V2:
             self._service["type"] = ServiceType.BITBUCKET_V2
             if "/1.0" in self._service["url"]:
                 self._service["type"] = ServiceType.BITBUCKET_V1
-
-        # Set timeout for all requests.
-        # See: https://github.com/psf/requests/issues/2011#issuecomment-490050252
-        self._request = requests.Session()
-        self._request.request = functools.partial(self._request.request, timeout=SERVICE_AUTH_CHECK_TIMEOUT)
-
-        self._reachable = False
-        self._auth_successful = False
-        self._auth_type = AuthType.SVC
-        self._error = None
 
     def to_dict(self) -> ServiceConnectionStatus:
         client = get_memcache_client()
@@ -69,26 +48,14 @@ class Service:
             log.info(f"Cache hit for service: {key}")
             return json.loads(result.decode())
 
-        self._test_auth()
-        connection_status: ServiceConnectionStatus = {
+        return {
             "service": self.name,
             "service_type": self._service["type"],
-            "reachable": self._reachable,
-            "auth_successful": self._auth_successful,
+            "reachable": False,
+            "auth_successful": False,
             "auth_type": self._auth_type.value,
-            "error": self._error,
+            "error": f"Could not find entry for {self.name}",
         }
-
-        if not self._auth_successful or not self._reachable:
-            # Cache negative results for 10 minutes
-            log.info(f"Caching negative result for 10 minutes for {key}")
-            client.set(key, json.dumps(connection_status), 600)
-        else:
-            log.info(f"Caching positive result for 30 minutes for {key}")
-            # Cache positive results for 30 minutes
-            client.set(key, json.dumps(connection_status), 1800)
-
-        return connection_status
 
     def stats_to_dict(self, scope: list[list[list[str]]]):
         self._get_service_stats(scope)
@@ -103,16 +70,6 @@ class Service:
                 "latest_scan": self._latest_scan_timestamp,
             },
         }
-
-    def _set_request_fail(self, error: str = None):
-        """
-        Set the failure state for a request error, with an optional error message.
-
-        Request errors are considered to be reachability failures for the purposes of system status.
-        """
-        self._reachable = False
-        self._auth_successful = False
-        self._error = error
 
     def _get_service_stats(self, scope: list[list[list[str]]]):
         repos = Repo.in_scope(scope).filter(service=self._service_name).order_by("repo")
@@ -140,187 +97,3 @@ class Service:
             return ret
         except User.DoesNotExist:
             return []
-
-    def _test_auth(self):
-        key = get_api_key(self._service["secret_loc"])
-
-        # We are intentionally high-level in the error message returned to the
-        # user -- we don't control what potentially-sensitive information
-        # might be contained in the request error details.
-        try:
-            if key is None:
-                self._error = "Unable to retrieve key"
-            elif self._service["type"] == ServiceType.GITHUB:
-                self._test_github(key)
-            elif self._service["type"] == ServiceType.GITLAB:
-                self._test_gitlab(key)
-            elif self._service["type"] == ServiceType.BITBUCKET_V1:
-                self._test_bitbucket_v1(key)
-            elif self._service["type"] == ServiceType.BITBUCKET_V2:
-                self._test_bitbucket_v2(key)
-            elif self._service["type"] == ServiceType.ADO:
-                self._test_ado(key)
-        except requests.Timeout:
-            self._set_request_fail("Timeout")
-        except requests.ConnectionError:
-            self._set_request_fail("Connection error")
-        except requests.RequestException:
-            self._set_request_fail("Request failed")
-
-    def _test_github(self, key: str):
-        self._request.get(url=self._service["url"], timeout=3)
-        self._reachable = True
-
-        if self._org is not None:
-            # Attempt to get an app installation token for the organization
-            github_app = GithubApp()
-            token = github_app.get_installation_token(self._org, bypass_cache=True)
-            if token is not None:
-                self._auth_successful = True
-                self._auth_type = AuthType.APP
-                self._error = None
-                return
-
-        # Fall back to getting the PAT
-        revproxy = False
-        headers = {"Authorization": "bearer %s" % key, "Content-Type": "application/json"}
-        if REV_PROXY_DOMAIN_SUBSTRING and REV_PROXY_DOMAIN_SUBSTRING in self._service["url"]:
-            headers[REV_PROXY_SECRET_HEADER] = GetProxySecret()
-            revproxy = True
-
-        query = ""
-        variables = {}
-        if self._org is not None:
-            query = "query getLogin($org: String!) {organization(login: $org) {login}}"
-            variables["org"] = self._org
-        else:
-            query = "viewer { login }"
-
-        response = self._request.post(
-            url=self._service["url"],
-            headers=headers,
-            json={"query": query, "variables": variables},
-            timeout=3,
-        )
-        if response.status_code == 200:
-            self._auth_successful = True
-            self._auth_type = AuthType.SVC
-        else:
-            self._auth_successful = False
-            if revproxy and response.status_code == 401 and response.text == "key is invalid":
-                # Request did not make it through the reverse proxy
-                self._reachable = False
-                self._error = "Reverse proxy authentication failure"
-
-    def _test_gitlab(self, key: str):
-        revproxy = False
-        headers = {"Authorization": "Bearer %s" % key, "Content-Type": "application/json"}
-        if REV_PROXY_DOMAIN_SUBSTRING and REV_PROXY_DOMAIN_SUBSTRING in self._service["url"]:
-            headers[REV_PROXY_SECRET_HEADER] = GetProxySecret()
-            revproxy = True
-
-        response = self._request.post(
-            url=self._service["url"], headers=headers, json={"query": 'echo(text: "foo")'}, timeout=3
-        )
-        self._reachable = True
-        if response.status_code == 200:
-            echo = response.json().get("data", {}).get("echo", "")
-            if echo == "nil says: foo":
-                self._auth_successful = False
-            else:
-                self._auth_successful = True
-        else:
-            self._auth_successful = False
-            if revproxy and response.status_code == 401 and response.text == "key is invalid":
-                # Request did not make it through the reverse proxy
-                self._reachable = False
-                self._error = "Reverse proxy authentication failure"
-
-    def _test_bitbucket(self, key: str, service_auth_url: str, repo_auth_url: str):
-        revproxy = False
-        headers = {"Authorization": "Basic %s" % key, "Accept": "application/json"}
-        if REV_PROXY_DOMAIN_SUBSTRING and REV_PROXY_DOMAIN_SUBSTRING in self._service["url"]:
-            headers[REV_PROXY_SECRET_HEADER] = GetProxySecret()
-            revproxy = True
-
-        response = self._request.get(url=service_auth_url, headers=headers, timeout=3)
-        self._reachable = True
-        if response.status_code == 200:
-            if not self._org:
-                self._auth_successful = True
-            else:
-                response = self._request.get(
-                    url=repo_auth_url,
-                    headers=headers,
-                    timeout=3,
-                )
-                if response.status_code == 200 and response.json().get("size", 0) == 1:
-                    self._auth_successful = True
-                else:
-                    self._auth_successful = False
-                    if revproxy and response.status_code == 401 and response.text == "key is invalid":
-                        # Request did not make it through the reverse proxy
-                        self._reachable = False
-                        self._error = "Reverse proxy authentication failure"
-        else:
-            self._auth_successful = False
-            if revproxy and response.status_code == 401 and response.text == "key is invalid":
-                # Request did not make it through the reverse proxy
-                self._reachable = False
-                self._error = "Reverse proxy authentication failure"
-
-    def _test_bitbucket_v1(self, key: str):
-        repo_auth_url = ""
-        if self._org:
-            org, repo = self.org.split("/", 1)
-            repo_auth_url = f"{self._service['url']}/projects/{org}/repos/{repo}"
-
-        service_auth_url = f"{self._service['url']}/projects"
-        self._test_bitbucket(key, service_auth_url, repo_auth_url)
-
-    def _test_bitbucket_v2(self, key: str):
-        url = self._service["url"]
-        repo_auth_url = ""
-        if self._org:
-            repo_auth_url = f'{url}/user/permissions/workspaces?q=workspace.slug="{self._org}"'
-
-        service_auth_url = f"{url}/user"
-        self._test_bitbucket(key, service_auth_url, repo_auth_url)
-
-    def _test_ado(self, key: str):
-        headers = {"Authorization": "Basic %s" % _base64_encode(key), "Accept": "application/json"}
-        response = self._request.get(f"{self._service['url']}/{self._org}/_apis/projects", headers=headers)
-        self._reachable = True
-        if response.status_code == 200:
-            self._auth_successful = True
-        else:
-            self._auth_successful = False
-
-
-class GetProxySecret:
-    _secret = None
-
-    def __new__(cls):
-        if not cls._secret:
-            from artemislib.aws import AWSConnect  # pylint: disable=import-outside-toplevel
-
-            aws_connect = AWSConnect(region=REV_PROXY_SECRET_REGION)
-            cls._secret = aws_connect.get_secret_raw(REV_PROXY_SECRET)
-        return cls._secret
-
-
-def get_api_key(service_secret):
-    from artemislib.aws import AWSConnect, ClientError  # pylint: disable=import-outside-toplevel
-
-    aws_connect = AWSConnect()
-    try:
-        secret = aws_connect.get_secret(f"{APPLICATION}/{service_secret}")
-        if secret:
-            return secret.get("key")
-    except ClientError:
-        return None
-    return None
-
-
-def _base64_encode(text: str, input_encoding="utf-8", output_encoding="utf-8") -> str:
-    return base64.b64encode(bytes(text, input_encoding)).decode(output_encoding)
